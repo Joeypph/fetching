6c4956c268495a474ef7ef319ff4756e4e465625
Joeypph <joseant_1098@hotmail.com>
2022-05-03 10:20:57 -0500

    adding notification so that user can understand when to scroll

--- src/components/PeopleList.jsx
+++ src/components/PeopleList.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import PeopleItem from './PeopleItem'
 import "../App.css";
+
 const PeopleList = ({data}) => {
+  const [hidden, setHidden] = useState(true)
+
+  useEffect(() => {
+    visible()
+  },[data])
+
+  const visible =()=>{
+    setHidden(false);
+    setTimeout(() => {
+      setHidden(true)
+      console.log("Se cierra")
+    }, 3000);
+    console.log(hidden)
+  }
+  
   return (
+    <>
     <div className="flowProfile">
-        {data.length > 0 ? 
+        {data.length > 0 ? (
+
           data.map(person =>
             <PeopleItem key={person.birth_year + person.name} name={person.name}/>)
+          )
             :
             <h5>No hay info</h5>
         }
     </div>
+    <div> 
+     {data.length > 10 && (
+      <h3 hidden={hidden} style={{color:'green'}}>Deslice para ver los elementos cargados &#x2193; </h3>
+      )}
+    </div>
+    </>
   )
 }
 
